799ccdbb5c203907bb74fa91653fd766eb3abe76
Abhishekgitok <abhishek19781@gmail.com>
2024-05-08 12:58:12 +0530

    some change

--- controllers/productController.js
+++ controllers/productController.js
@@ -204,6 +204,7 @@ export const productFiltersController = async (req, res) => {
     const { checked, radio } = req.body;
     let args = {};
     if (checked.length > 0) args.category = checked;
+    // gte means greater then and lte less then
     if (radio.length) args.price = { $gte: radio[0], $lte: radio[1] };
     const products = await productModel.find(args);
     res.status(200).send({
